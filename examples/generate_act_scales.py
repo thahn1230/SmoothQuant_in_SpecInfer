cf7c55c2b31b9381cd8bafa03609e807be552bfa
sungsomi <thahn1230@naver.com>
2025-03-07 08:29:45 +0000

    add calib data pt

--- examples/generate_act_scales.py
+++ examples/generate_act_scales.py
@@ -1,4 +1,13 @@
 import torch
+import torch.nn as nn
+from datasets import load_dataset
+import functools
+from collections import defaultdict
+
+from functools import partial
+import numpy as np
+from tqdm import tqdm
+
 import os
 
 from transformers import (
@@ -7,7 +16,47 @@ from transformers import (
 )
 import argparse
 
-from smoothquant.calibration import get_act_scales
+
+def get_act_scales(model, tokenizer, dataset_path, num_samples=512, seq_len=512):
+    model.eval()
+    device = next(model.parameters()).device
+    act_scales = {}
+
+    def stat_tensor(name, tensor):
+        hidden_dim = tensor.shape[-1]
+        tensor = tensor.view(-1, hidden_dim).abs().detach()
+        comming_max = torch.max(tensor, dim=0)[0].float().cpu()
+        if name in act_scales:
+            act_scales[name] = torch.max(act_scales[name], comming_max)
+        else:
+            act_scales[name] = comming_max
+
+    def stat_input_hook(m, x, y, name):
+        if isinstance(x, tuple):
+            x = x[0]
+        stat_tensor(name, x)
+
+    hooks = []
+    for name, m in model.named_modules():
+        if isinstance(m, nn.Linear):
+            hooks.append(
+                m.register_forward_hook(functools.partial(stat_input_hook, name=name))
+            )
+
+    dataset = load_dataset("json", data_files=dataset_path, split="train")
+    dataset = dataset.shuffle(seed=42)
+
+    for i in tqdm(range(num_samples)):
+        input_ids = tokenizer(
+            dataset[i]["text"], return_tensors="pt", max_length=seq_len, truncation=True
+        ).input_ids.to(device)
+        model(input_ids)
+
+    for h in hooks:
+        h.remove()
+
+    return act_scales
+
 
 
 def build_model_and_tokenizer(model_name):
